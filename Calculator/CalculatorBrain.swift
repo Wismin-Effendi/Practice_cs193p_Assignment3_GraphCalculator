243b40709723797e775f9259d6801d8d4f3026a4
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-21 19:02:01 -0500

    Implement Assignment 1 solution as per Michel Deiman's GitHub +  fix for setOperand should not be set when Operation is Constant (like π, e, Rand)

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,22 +8,96 @@
 
 import Foundation
 
-func changeSign(operand: Double) -> Double {
-    return -operand
-}
-
-func multiply(op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
 
 struct CalculatorBrain {
     
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+    var resultIsPending: Bool {
+        return pendingBinaryOperation != nil
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        if !resultIsPending {
+            clear()
+        }
+        accumulator = operand
+        descriptions.append(formattedAccumulator!)
+    }
+    
+    mutating func performOperation(_ symbol: String) {
+        guard let operation = operations[symbol]  else { return }
+        switch operation {
+        case .constant(let value):
+            accumulator = value
+            descriptions.append(symbol)
+        case .unaryOperation(let function):
+            if let operand = accumulator {
+                if resultIsPending {
+                    let lastOperand = descriptions.last!
+                    descriptions = [String](descriptions.dropLast()) + [symbol + "(" + lastOperand + ")"]
+                } else {
+                    descriptions = [symbol + "("] + descriptions + [")"]
+                }
+                accumulator = function(operand)
+            }
+        case .binaryOperation(let function):
+            if resultIsPending {
+                performPendingBinaryOperation()
+            }
+            if accumulator != nil {
+                pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                accumulator = nil
+                descriptions.append(symbol)
+            }
+        case .noArgumentOperation(let function):
+            accumulator = function()
+            descriptions.append(symbol)
+        case .equals:
+            performPendingBinaryOperation()
+        }
+    }
+    
+    mutating func clear() {
+        accumulator = nil
+        pendingBinaryOperation = nil
+        descriptions = []
+    }
+    
+    weak var numberFormatter: NumberFormatter?
+    
+    var description: String {
+        var returnString: String = ""
+        for element in descriptions {
+            returnString += element
+        }
+        return returnString
+    }
+    
+    
+    // MARK: - Private properties and methods
+
+    private var descriptions: [String] = []
     private var accumulator: Double?
+
+    private var formattedAccumulator: String? {
+        if let number = accumulator {
+            return numberFormatter?.string(from: number as NSNumber) ?? String(number)
+        } else {
+            return nil
+        }
+    }
     
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double,Double) -> Double)
+        case noArgumentOperation(()-> Double)
         case equals
     }
     
@@ -31,40 +105,25 @@ struct CalculatorBrain {
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
         "√" : Operation.unaryOperation(sqrt),
+        "1/x" : Operation.unaryOperation({ 1/$0 }),
+        "%": Operation.unaryOperation({ $0/100 }),
         "cos" : Operation.unaryOperation(cos),
-        "∓" : Operation.unaryOperation({ -$0 }),
-        "x": Operation.binaryOperation({ $0 * $1 }),
-        "÷": Operation.binaryOperation({ $0 / $1 }),
-        "+": Operation.binaryOperation({ $0 + $1 }),
-        "-": Operation.binaryOperation({ $0 - $1 }),
+        "sin": Operation.unaryOperation(sin),
+        "tan": Operation.unaryOperation(tan),
+        "Ran": Operation.noArgumentOperation( { Double(drand48()) }),
+        "±" : Operation.unaryOperation({ -$0 }),
+        "x": Operation.binaryOperation(*),
+        "÷": Operation.binaryOperation(/),
+        "+": Operation.binaryOperation(+),
+        "-": Operation.binaryOperation(-),
         "=": Operation.equals
     ]
-    
-    mutating func performOperation(_ symbol: String) {
-        if let operation = operations[symbol] {
-            switch operation {
-            case .constant(let value):
-                accumulator = value
-            case .unaryOperation(let function):
-                if accumulator != nil {
-                    accumulator = function(accumulator!)
-                }
-            case .binaryOperation(let function):
-                if accumulator != nil {
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
-                    accumulator = nil
-                }
-            case .equals:
-                performPendingBinaryOperation()
-            }
-        }
-    }
+
     
     private mutating func performPendingBinaryOperation() {
-        if pendingBinaryOperation != nil && accumulator != nil {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
-            pendingBinaryOperation = nil
-        }
+        guard pendingBinaryOperation != nil && accumulator != nil  else { return }
+        accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+        pendingBinaryOperation = nil
     }
     
     private var pendingBinaryOperation: PendingBinaryOperation?
@@ -78,13 +137,5 @@ struct CalculatorBrain {
         }
     }
     
-    mutating func setOperand(_ operand: Double) {
-        accumulator = operand
-    }
-    
-    var result: Double? {
-        get {
-            return accumulator
-        }
-    }
+
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,25 +11,53 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var decimalSeparator: UIButton!
     
     var userIsInTheMiddleOfTyping = false
-    var dotHasBeenEntered = false
+    
+    private var brain = CalculatorBrain()
+    private var numberFormatter = NumberFormatter()
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = numberFormatter.string(from: newValue as NSNumber)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+        numberFormatter.alwaysShowsDecimalSeparator = false
+        numberFormatter.maximumFractionDigits = 6
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.minimumIntegerDigits = 1
+        decimalSeparator.setTitle(numberFormatter.decimalSeparator, for: .normal)
+        
+        brain.numberFormatter = numberFormatter
+    }
+    
+    @IBAction func backSpace(_ sender: UIButton) {
+        guard userIsInTheMiddleOfTyping else { return }
+        display.text = String(display.text!.characters.dropLast())
+        if display.text?.characters.count == 0 {
+            displayValue = 0.0
+            userIsInTheMiddleOfTyping = false
+        }
     }
 
+    @IBAction func clearAll(_ sender: UIButton) {
+        brain.clear()
+        displayValue = 0.0
+        history.text = " "
+    }
+    
 
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        if digit == "." {
-            if dotHasBeenEntered {
-                return
-            }
-            dotHasBeenEntered = true
-        }
-        
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text!
             display.text = textCurrentlyInDisplay + digit
@@ -40,24 +68,28 @@ class ViewController: UIViewController {
     }
     
 
-    var displayValue: Double {
-        get {
-            return Double(display.text!)!
+    @IBAction func floatingPoint(_ sender: UIButton) {
+        if !userIsInTheMiddleOfTyping {
+            display.text = "0" + numberFormatter.decimalSeparator
+        } else if !display.text!.contains(numberFormatter.decimalSeparator) {
+            display.text = display.text! + numberFormatter.decimalSeparator
         }
-        set {
-            display.text = String(newValue)
-        }
+        userIsInTheMiddleOfTyping = true 
     }
+    
 
-    private var brain = CalculatorBrain()
     
     @IBAction func performOperation(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping {
-            brain.setOperand(displayValue)
+            // if sender not any of the constant, i.e. π or e or Ran then setOperand else skip
+            if let title = sender.currentTitle,
+                !Set(["π", "e", "Ran"]).contains(title) {
+                brain.setOperand(displayValue)
+            }
             userIsInTheMiddleOfTyping = false
-            dotHasBeenEntered = false 
         }
         
+        print(brain.description)
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
@@ -65,7 +97,11 @@ class ViewController: UIViewController {
         if let result = brain.result {
             displayValue = result
         }
+        let postfixDescription = brain.resultIsPending ? "..." : "="
+        history.text = brain.description + postfixDescription
     }
+    
+    
 
 }
 
